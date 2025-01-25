d1f3d3081d8b0c1a81b76646abba93aa223130ab
Diego Pedroso <96250748+DiegoSPPedroso@users.noreply.github.com>
2025-01-25 13:46:47 -0300

    Delete support directory

--- support/data.jsx
+++
@@ -1,9 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export const dataToUser = () => {
-    const data = {
-        name: faker.person.fullName(),
-        job: faker.person.jobTitle()
-    };
-    return data;
-};
